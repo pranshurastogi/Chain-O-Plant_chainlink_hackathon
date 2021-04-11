3959aae2e7e4bdba0e19a09bab8f2eea8511bd5f
ishitarastogi <ishita3099@gmail.com>
2021-04-11 17:29:54 +0530

    change links

--- ChainLink/migrations/2_deploy_contract.js
+++
@@ -1,8 +0,0 @@
-const BettingGame = artifacts.require('./BettingGame.sol')
-
-module.exports = async deployer => {
-  await deployer.deploy(BettingGame)
-  const bettingGame= await BettingGame.deployed(
-      console.log(bettingGame.address)
-  )
-}
---
+++ front-end/README.md
@@ -0,0 +1,2 @@
+# Chain-O-Plant_chainlink_hackathon
+Chain-O-Plant where O stands for the ecosystem, it's a dapp focused to boost environment and local art that is getting lost in this supercomputer era.
